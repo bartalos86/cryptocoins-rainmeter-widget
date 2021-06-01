14641b182dc760202604ac6f7a454093819f01be
Bartalos Mark <bartalos86@gmail.com>
2021-06-01 21:13:47 +0200

    Number rounding added back

--- @Resources/scripts/Helpers.lua
+++ @Resources/scripts/Helpers.lua
@@ -12,37 +12,21 @@ function Round(num)
         return
     end
 
-
     if num == '' then
         return num
     end
     
-    num = tonumber(num)
+    local number = tonumber(num)
     
     local idp = 2
-    if num < 1 then
-        idp = 6
+    if number < 1.0 then
+        return num
     end
         
 	local mult = 10 ^ (idp or 0)
-	if num >= 0 then
-		return math.floor(num * mult + 0.5) / mult
+	if number >= 0 then
+		return math.floor(number * mult + 0.5) / mult
 	else
-		return math.ceil(num * mult - 0.5) / mult
+		return math.ceil(number * mult - 0.5) / mult
 	end
 end
-
--- function CheckVersion(current, latest)
---     local MeasureVersion = SKIN:GetMeasure('MeasureVersion')
-    
---     current = tonumber(SKIN:GetVariable('Version'))
---     latest = tonumber(MeasureVersion:GetStringValue())
-    
---     local MeterDonateIcon = SKIN:GetMeter('MeterDonateIcon')
---     local MeterUpgradeIcon = SKIN:GetMeter('MeterUpgradeIcon')
---     if latest > current then
---         MeterUpgradeIcon:Show()
---     else
---         MeterDonateIcon:Show()
---     end
--- end
