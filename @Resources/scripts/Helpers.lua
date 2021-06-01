d35f305845370e2ced7bbde1005ca6bd828f5e9f
Bartalos Mark <bartalos86@gmail.com>
2021-06-01 18:44:32 +0200

    initial commit

---
+++ @Resources/scripts/Helpers.lua
@@ -0,0 +1,48 @@
+function Initialize()
+    
+end
+
+function ToPercent(num)
+    return (tonumber(num)*100.0)
+end
+
+function Round(num)
+
+    if num == nil then
+        return
+    end
+
+
+    if num == '' then
+        return num
+    end
+    
+    num = tonumber(num)
+    
+    local idp = 2
+    if num < 1 then
+        idp = 6
+    end
+        
+	local mult = 10 ^ (idp or 0)
+	if num >= 0 then
+		return math.floor(num * mult + 0.5) / mult
+	else
+		return math.ceil(num * mult - 0.5) / mult
+	end
+end
+
+-- function CheckVersion(current, latest)
+--     local MeasureVersion = SKIN:GetMeasure('MeasureVersion')
+    
+--     current = tonumber(SKIN:GetVariable('Version'))
+--     latest = tonumber(MeasureVersion:GetStringValue())
+    
+--     local MeterDonateIcon = SKIN:GetMeter('MeterDonateIcon')
+--     local MeterUpgradeIcon = SKIN:GetMeter('MeterUpgradeIcon')
+--     if latest > current then
+--         MeterUpgradeIcon:Show()
+--     else
+--         MeterDonateIcon:Show()
+--     end
+-- end
